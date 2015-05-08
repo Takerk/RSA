384e34506ec86a3025987953e3d01a625df11eab
David Espinel <undertaker23242@gmail.com>
2015-05-07 22:13:48 -0500

    - Agregados metodos de listabinario y powermod

--- primesGeneration.py
+++ primesGeneration.py
@@ -20,14 +20,46 @@ inicio=1000000
 print('Generacion de p y q, ambos primos')
 p=0
 q=0
-print(esprimo(999769))
-encontrado=0
-n=inicio
-while(n<=limite):
-    if esprimo(n):
-        encontrado=n
-        print(encontrado)
-    n=n+1
+#print(esprimo(999769))
+#encontrado=0
+#n=inicio
+#while(n<=limite):
+ #   if esprimo(n):
+  #      encontrado=n
+   #     print(encontrado)
+    #n=n+1
+
+def listabinario(decimal):
+    lista=[]
+    binario=bin(decimal)
+    for i in binario:
+        lista.append(i)
+    del lista[0]
+    del lista[0]
+    return lista
+
+
+lbinario=listabinario(10949)
+print(lbinario)
+n=27641
+
+def powermod(numero):
+    base=numero
+    del lbinario[0]
+    for i in lbinario:
+        if i=='1':
+            print("entro a 1")
+            numero=(numero*numero*base)%n
+        else:
+            print("entro a 0")
+            numero=(numero*numero)%n
+        print("el valor de i es: " ,i ," el numero es: ",numero)
+    return numero
+
+print(powermod(45))
+
+
+
 
 
 
